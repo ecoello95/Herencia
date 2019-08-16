36fc939f58a5c840be9c937e7e12795e3ad3c090
eduar <eduar@DESKTOP-EV5A97R>
2019-08-16 11:57:13 -0600

    creando herencia

---
+++ Therencia/Therencia/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Therencia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var heroe1 = new Heroe();
+            heroe1.Id = 01;
+            heroe1.Nombre = "Goku";
+            heroe1.Poderes = "Kamehameha";
+
+
+            var heroe2 = new Heroe();
+            heroe2.Id = 01;
+            heroe2.Nombre = "Gohan";
+            heroe2.Poderes = "Masenko";
+
+
+            var heroe3 = new Heroe();
+            heroe3.Id = 01;
+            heroe3.Nombre = "Vegeta";
+            heroe3.Poderes = "Resplandor Final";
+
+            var villano1 = new Villano();
+            villano1.Id = 02;
+            villano1.Nombre = "Freezer";
+            villano1.Poderes = "Destruir Planetas";
+
+            var villano2 = new Villano();
+            villano2.Id = 02;
+            villano2.Nombre = "Cell";
+            villano2.Poderes = "Absorber";
+
+            var villano3 = new Villano();
+            villano3.Id = 03;
+            villano3.Nombre = "Magneto";
+            villano3.Poderes = "Control de metales";
+        }
+    }
+}
---
+++ Therencia/Therencia/Super_Poderes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Therencia
+{
+   public class Super_Poderes: Personaje
+    {
+        public string Poderes { get; set; }
+    }
+}
